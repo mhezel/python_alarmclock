2ce8faef2a5b7acd746dc7c2decf7817b0633564
Mhezel Mohammad <mhezelkhan@gmail.com>
2023-02-21 16:11:18 +0800

    alarm clock/timer

---
+++ clock.py
@@ -0,0 +1,29 @@
+from playsound import playsound
+import time
+
+CLEAR = "\033[2J"
+CLEAR_AND_RETURN = "\033[2H"
+
+#playsound("Clock-sound-effect.mp3")
+
+def alarm(seconds):
+    time_elapsed = 0
+
+    print(CLEAR)
+    while time_elapsed < seconds:
+        time.sleep(1)
+        time_elapsed += 1
+
+        time_left = seconds - time_elapsed
+        minutes_left = time_left // 60 #integer division
+        seconds_left = time_left % 60 #remainder
+
+        print(f"{CLEAR_AND_RETURN}Alarm will sound in: {minutes_left:02d}:{seconds_left:02d}")
+
+    #playsound("Clock-sound-effect.mp3")
+
+minutes = int(input("How many minutes: "))
+seconds = int(input("How many seconds: "))
+total_seconds = minutes * 60 + seconds
+
+alarm(total_seconds)
