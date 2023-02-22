60a8673d3bdb33f987fc69c9265adfd1edd5fe04
Mhezel Mohammad <mhezelkhan@gmail.com>
2023-02-22 21:16:21 +0800

    add new alarm sound and import playsound package from pip using conda

--- clock.py
+++ clock.py
@@ -4,11 +4,8 @@ import time
 CLEAR = "\033[2J"
 CLEAR_AND_RETURN = "\033[2H"
 
-#playsound("Clock-sound-effect.mp3")
-
 def alarm(seconds):
     time_elapsed = 0
-
     print(CLEAR)
     while time_elapsed < seconds:
         time.sleep(1)
@@ -19,11 +16,12 @@ def alarm(seconds):
         seconds_left = time_left % 60 #remainder
 
         print(f"{CLEAR_AND_RETURN}Alarm will sound in: {minutes_left:02d}:{seconds_left:02d}")
+    playsound("alarm.mp3")
 
-    #playsound("Clock-sound-effect.mp3")
 
 minutes = int(input("How many minutes: "))
 seconds = int(input("How many seconds: "))
 total_seconds = minutes * 60 + seconds
 
 alarm(total_seconds)
+
